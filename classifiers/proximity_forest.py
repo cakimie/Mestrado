48fc3919a5ae8c22716e8e9f0d08b862ce576c52
Carolina <carolina.kimie@gmail.com>
2024-05-07 18:00:37 +0900

    Fixes package detection by ClearML.

--- classifiers/proximity_forest.py
+++ classifiers/proximity_forest.py
@@ -20,5 +20,41 @@ def proximity_forest (X_train, y_train, X_test, y_test):
     }
 
 if __name__ == '__main__':
-    from classifiers.run_classifier import run_classifier
-    run_classifier(proximity_forest, 'proximity_forest')
+    import time
+    start_time = time.time()
+
+    import numpy as np
+    import pandas as pd
+    from classifiers.load_fold import load_fold
+
+    from clearml import Task
+    params = {
+        'k': 1,
+        'K': 10,
+        'country': 0,
+        'city': 0,
+        'category': None,
+    }
+    task = Task.init(project_name='PopularTimesFold/Classifier', task_name="proximity_forest")
+    task.connect(params)
+
+    df = pd.read_csv('weekdays_datasets/df_timeseries.csv')
+    name, X_train, y_train, X_test, y_test = load_fold(
+        df,
+        params['k'],
+        params['K'],
+        params['country'],
+        params['city'],
+        params['category'],
+    )
+    print(f'Loaded: {name}')
+    
+    # Executes main function:
+    main_time = time.time()
+    results = proximity_forest(X_train, y_train, X_test, y_test)
+    task.get_logger().report_scalar('execution_time', 'main', iteration=0, value=time.time() - main_time)
+
+    # Reports results:
+    for key, value in results.items():
+        task.get_logger().report_scalar('metrics', key, iteration=0, value=value)
+    task.get_logger().report_scalar('execution_time', 'total', iteration=0, value=time.time() - start_time)
--- classifiers/ts_fresh.py
+++ classifiers/ts_fresh.py
@@ -22,5 +22,41 @@ def ts_fresh (X_train, y_train, X_test, y_test):
     }
 
 if __name__ == '__main__':
-    from classifiers.run_classifier import run_classifier
-    run_classifier(ts_fresh, 'ts_fresh')
+    import time
+    start_time = time.time()
+
+    import numpy as np
+    import pandas as pd
+    from classifiers.load_fold import load_fold
+
+    from clearml import Task
+    params = {
+        'k': 1,
+        'K': 10,
+        'country': 0,
+        'city': 0,
+        'category': None,
+    }
+    task = Task.init(project_name='PopularTimesFold/Classifier', task_name="ts_fresh")
+    task.connect(params)
+
+    df = pd.read_csv('weekdays_datasets/df_timeseries.csv')
+    name, X_train, y_train, X_test, y_test = load_fold(
+        df,
+        params['k'],
+        params['K'],
+        params['country'],
+        params['city'],
+        params['category'],
+    )
+    print(f'Loaded: {name}')
+    
+    # Executes main function:
+    main_time = time.time()
+    results = ts_fresh(X_train, y_train, X_test, y_test)
+    task.get_logger().report_scalar('execution_time', 'main', iteration=0, value=time.time() - main_time)
+
+    # Reports results:
+    for key, value in results.items():
+        task.get_logger().report_scalar('metrics', key, iteration=0, value=value)
+    task.get_logger().report_scalar('execution_time', 'total', iteration=0, value=time.time() - start_time)
